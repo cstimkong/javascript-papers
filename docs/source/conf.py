9143142d646c5ff3544fbbce2aa3cdbe3411923d
Dezhen Kong <timkong@zju.edu.cn>
2026-01-02 19:09:46 +0800

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'JavaScript Papers'
+copyright = '2025, Dezhen Kong'
+author = 'Dezhen Kong'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0.0'
+version = '1.0.0'
 
 # -- General configuration
 
